655948672a1f11a8a43e704d555fd0eda66fd5d6
cymjoe <312400179@qq.com>
2020-06-18 10:43:52 +0800

    刪除無用代碼

--- lib-base/src/main/java/com/cymjoe/lib_base/constant/Constant.kt
+++ lib-base/src/main/java/com/cymjoe/lib_base/constant/Constant.kt
@@ -6,7 +6,6 @@ object Constant {
         "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCq6gajnq/rS6tRhqYgsD5H1c/oyja1b8n4oybqYErHzvIsnxWIpGXJ5ozXshObQpNGOHqCTWDjlTYp5I5AdMGdgyWterWwkI43YsevOkHldksgU0KU3WwNfzfXAfmq5UFO0xCn19tkZn/PDf+iP5B6NOx7qhkxPOwsoim7rfZp/QIDAQAB"
 
     //baseUrl
-    val BASE_URL: String? = "https://api.chunjingtech.com/#/"
-    const val LOGIN = "login"
+
 
 }
